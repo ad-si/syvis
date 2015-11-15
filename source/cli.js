c15c91abec5530d67c0faef59ece1f6bbb7f4318
Adrian Sieber <mail@adriansieber.com>
2015-11-15 20:42:39 +0100

    Add Favicon

--- source/cli.js
+++ source/cli.js
@@ -8,10 +8,13 @@ const fs = require('fs')
 const esprima = require('esprima')
 const express = require('express')
 const stylus = require('stylus')
+const serveFavicon = require('serve-favicon')
 
 const app = express()
 const port = 3000
 
+
+app.use(serveFavicon(path.resolve(__dirname, '..', 'images', 'favicon.ico')))
 app.use(stylus.middleware({
 	src: __dirname,
 	dest: path.join(__dirname, '../public')
