716fa18c26fe9414690ec3514ad9c3e9b9527fdf
Adrian Sieber <mail@adriansieber.com>
2015-11-17 00:10:42 +0100

    Render colon-equal sign for assignments

--- source/visualizers/assignment-expression.js
+++ source/visualizers/assignment-expression.js
@@ -1,10 +1,8 @@
 const walkTree = require('../walkTree')
 
-console.log(walkTree)
-
 module.exports = (expression) => [
 	'section.assignmentExpression',
 	['span.left', walkTree(expression.left)],
-	['span.assignment'],
+	['span.assignment', '='],
 	['span.right', walkTree(expression.right)]
 ]
--- source/visualizers/variable-declarator.js
+++ source/visualizers/variable-declarator.js
@@ -3,6 +3,6 @@ const walkTree = require('../walkTree')
 module.exports = (node) => [
 	'p.declaration',
 	['span', walkTree(node.id)],
-	node.init ? ['span.assignment'] : true,
+	node.init ? ['span.assignment', '='] : true,
 	node.init ? ['span', walkTree(node.init)] : true
 ]
