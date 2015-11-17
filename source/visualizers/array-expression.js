2ae366d74d02f9712a58f776fd851a8569e1f0d7
Adrian Sieber <mail@adriansieber.com>
2015-11-17 11:32:59 +0100

    Improve rendering of ArrayExpression

--- source/visualizers/array-expression.js
+++ source/visualizers/array-expression.js
@@ -2,5 +2,14 @@ const walkTree = require('../walkTree')
 
 module.exports = (node) => [
 	'span.arrayExpression',
-	...node.elements.map(element => walkTree(element))
+	// ['span.openingBracket', '['],
+	...node.elements.map((element, index) =>
+		[
+			['span.arrayElement', walkTree(element)],
+			// (index !== node.elements.length - 1) ?
+			// 	['span.arraySeparator', ','] :
+			// 	''
+		]
+	),
+	// ['span.closingBracket', ']']
 ]
