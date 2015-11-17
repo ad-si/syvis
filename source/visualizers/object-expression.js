7cfa7ea02f32c4c58a90fdf9e73b2d00242281a9
Adrian Sieber <mail@adriansieber.com>
2015-11-17 11:30:43 +0100

    Add visualizer for ObjectExpression

---
+++ source/visualizers/object-expression.js
@@ -0,0 +1,8 @@
+const walkTree = require('../walkTree')
+
+module.exports = (node) => [
+	'span.objectExpression',
+	...node.properties.map(property =>
+		['span.property', walkTree(property)]
+	)
+]
