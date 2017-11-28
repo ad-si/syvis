92349a4f87e0de6fa994b74299e4820df1980632
Adrian Sieber <mail@adriansieber.com>
2017-11-28 22:26:48 +0000

    Add support for "for statement", improve design (especially colors)

--- source/visualizers/function-expression.js
+++ source/visualizers/function-expression.js
@@ -2,7 +2,7 @@ const walkTree = require('../walkTree')
 
 module.exports = (node) => [
   'span.functionExpression',
-  ['span.params', ...node.params.map(walkTree)],
+  ['span.parameters', ...node.params.map(walkTree)],
   ['span', ' => '],
   ['span', walkTree(node.body)],
 ]
--- source/visualizers/variable-declarator.js
+++ source/visualizers/variable-declarator.js
@@ -1,7 +1,7 @@
 const walkTree = require('../walkTree')
 
 module.exports = (node) => [
-  'p.declaration',
+  '.declaration',
   ['span', walkTree(node.id)],
   node.init ? ['span.assignment', '='] : true,
   node.init ? ['span', walkTree(node.init)] : true,
