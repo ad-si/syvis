8cc6827d7b6ac7f2b6720a58fe2a6cd81d579232
Adrian Sieber <mail@adriansieber.com>
2017-11-28 20:10:23 +0000

    Add support for if statement

---
+++ source/visualizers/if-statement.js
@@ -0,0 +1,12 @@
+const walkTree = require('../walkTree')
+
+module.exports = node => {
+  return [
+    'section.code.if',
+    ['header.test',
+      ['div', walkTree(node.test)],
+    ],
+    ['div.consequent', node.consequent ? walkTree(node.consequent) : null],
+    ['div.alternate', node.alternate ? walkTree(node.alternate) : null],
+  ]
+}
