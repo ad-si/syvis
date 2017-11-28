aec071f64640de0de69a54de511617dec80a32f2
Adrian Sieber <mail@adriansieber.com>
2017-11-28 18:43:01 +0000

    Add example files

---
+++ examples/add.js
@@ -0,0 +1,25 @@
+function add_a (x, y) {
+return x + y
+}
+
+function add_b
+(x,y)
+{
+return x +
+y
+}
+
+function add_c (
+x,
+y
+) {return x + y}
+
+function add_d(x,y){return x+y}
+
+
+console.log(
+  add_a(2, 5),
+  add_b(2, 5),
+  add_c(2, 5),
+  add_d(2, 5)
+)
