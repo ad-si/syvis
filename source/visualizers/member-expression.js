6fed25550dca684988c524f5742f23a7c5d3de40
Adrian Sieber <mail@adriansieber.com>
2017-11-30 19:32:50 +0000

    Render `this` in Ruby's `@` style

--- source/visualizers/member-expression.js
+++ source/visualizers/member-expression.js
@@ -3,6 +3,9 @@ const walkTree = require('../walkTree')
 module.exports = (node) => {
   const classes = ['memberExpression']
   if (node.computed) classes.push('computed')
+  if (node.object.type === 'ThisExpression') {
+    classes.push('containsThisExpression')
+  }
 
   return [
     'span',
