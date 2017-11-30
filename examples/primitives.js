2afdc236f8c6dc33fb0ac0edec6ba9aff8a6d100
Adrian Sieber <mail@adriansieber.com>
2017-11-30 07:50:29 +0000

    Add support for template strings, add examples file for primitives

---
+++ examples/primitives.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+
+const integer = 123
+const float = 123.456
+const notANumber = NaN
+const emptyString = ''
+const string = 'This is a string'
+// eslint-disable-next-line quotes
+const templateString = `This is also just a string`
+const templateWithExpressions = `This is a ${'VE' + 'RY'} special string`
+const object = {key: 'and value'}
+const array = ['item 1', 'item 2', 'item 3']
+const regex = /[0-9]{4}-([a-z]{5})?/gi
+const boolean = true
+const infinity = Infinity
+const undefinedValue = undefined
+const nullValue = null
+const voidValue = void 0
+const typeofValue = typeof 0
+const date = new Date('2017-11-29')
