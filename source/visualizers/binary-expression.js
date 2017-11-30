5761907372ae27f848dd6690a84e1cc223031edb
Adrian Sieber <mail@adriansieber.com>
2017-11-30 15:30:42 +0000

    Add multiplication operator, gracefully render missing operator

--- source/visualizers/binary-expression.js
+++ source/visualizers/binary-expression.js
@@ -1,11 +1,16 @@
 const walkTree = require('../walkTree')
 const operatorMap = require('../operatorMap.js')
 
-module.exports = (node) => [
-  'span.binaryExpression',
-  {
-    class: 'operator-' + (operatorMap.binary[node.operator] || ''),
-  },
-  ['span.left', walkTree(node.left)],
-  ['span.right', walkTree(node.right)],
-]
+module.exports = (node) => {
+  const classes = [
+    'binaryExpression',
+    'operator-' + (operatorMap.binary[node.operator] || ''),
+  ]
+
+  return [
+    'span',
+    {class: classes.join(' ')},
+    ['span.left', walkTree(node.left)],
+    ['span.right', walkTree(node.right)],
+  ]
+}
