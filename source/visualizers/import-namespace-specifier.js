c17770e48abd06915a08efeb334fd820c4e275f4
Adrian Sieber <mail@adriansieber.com>
2017-12-13 01:08:03 +0000

    Add remaining import statements

--- source/visualizers/import-namespace-specifier.js
+++ source/visualizers/import-namespace-specifier.js
@@ -1,6 +1,6 @@
 const walkTree = require('../walkTree')
 
 module.exports = (node) => [
-  'span.importDefaultSpecifier',
+  'span.importNamespaceSpecifier',
   ['span.local', walkTree(node.local)],
 ]
