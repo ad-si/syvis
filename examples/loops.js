2a0cb635a64a0c53215f3a87ce53e6f8b8e9bac0
Adrian Sieber <mail@adriansieber.com>
2018-01-22 00:07:43 +0000

    Add support for `for … in` statement

--- examples/loops.js
+++ examples/loops.js
@@ -2,9 +2,19 @@ for (let index = 0; index < 100; index++) {
   console.info(index)
 }
 
+
+const alphabet = {first: 'a', second: 'b', third: 'c'}
+
+for (const key in alphabet) {
+  if (!alphabet.hasOwnProperty(key)) continue
+  console.info(key, '=>', alphabet[key])
+}
+
+
 for (const value of [1, 2, 3]) {
   console.info(value)
 }
+
 
 let index = 0
 while (index < 100) {
